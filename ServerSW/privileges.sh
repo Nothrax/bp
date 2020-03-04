eb37b8d3d34fe9b18fdd270ef606c5f963efb7be
Jakub Trubka <trubka.jakub@gmail.com>
2020-03-04 12:59:35 +0100

    Update privileges.sh

--- ServerSW/privileges.sh
+++ ServerSW/privileges.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 databases=$(influx -username admin -password bp_influx -execute "show databases;")
-databases=$(echo "$databases" | grep -Eio '^dat_([0-9]|[a-f]){8}$')
+databases=$(echo "$databases" | grep -Eio '^([0-9]|[a-f]){8}$')
 
 for tmp in $databases
 do
